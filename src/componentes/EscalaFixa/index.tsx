2a122a989135c643cf1d859bfadcfe59fe389acc
Victor Manuel <victor3mcastro@gmail.com>
2025-01-04 13:20:22 -0300

    Att

--- src/componentes/EscalaFixa/index.tsx
+++ src/componentes/EscalaFixa/index.tsx
@@ -22,7 +22,7 @@ const EscalaFixa: React.FC = () => {
     { id: "86-2025-01-04-19h-Matriz", data: "2025-01-04", horario: "19h", local: "Matriz", padre: "Padre Ivan" },
 
     { id: "87-2025-01-05-07h-Matriz", data: "2025-01-05", horario: "07h", local: "Matriz", padre: "Padre Ivan" },
-    { id: "74-2025-01-05-07h-Matriz", data: "2025-01-05", horario: "07h", local: "Matriz", padre: "Padre Eudásio" },
+    { id: "74-2025-01-05-07h-Matriz", data: "2025-01-05", horario: "07h", local: "Divino", padre: "Padre Eudásio" },
     { id: "75-2025-01-05-09h-Matriz", data: "2025-01-05", horario: "09h", local: "Matriz", padre: "Padre Eudásio" },
     { id: "88-2025-01-05-09h-Sao-Jose", data: "2025-01-05", horario: "09h", local: "São José", padre: "Padre Ivan" },
     { id: "76-2025-01-05-11h-Sitio-Marista", data: "2025-01-05", horario: "11h", local: "Sítio Marista (Ação de Graças)", padre: "Padre Eudásio" },
