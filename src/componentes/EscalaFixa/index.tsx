e6bb5bb4e7abe01b983f8d38c21309984afaae6e
Victor Manuel <victor3mcastro@gmail.com>
2025-01-22 17:23:53 -0300

    ATT

--- src/componentes/EscalaFixa/index.tsx
+++ src/componentes/EscalaFixa/index.tsx
@@ -19,7 +19,7 @@ const EscalaFixa: React.FC = () => {
     { id: "78-2025-01-22-19h-SantosDumont", data: "2025-01-22", horario: "19h", local: "Santos Dumont", padre: "Padre Ivan" },
     { id: "78-2025-01-22-19h-Matriz-MissaFamilias", data: "2025-01-22", horario: "19h", local: "Matriz (Missa pelas Famílias)", padre: "Padre Eudásio" },
 
-    { id: "78-2025-01-23-MaeRainha", data: "2025-01-23", horario: "", local: "Mãe Rainha", padre: "Padre Ivan" },
+    { id: "78-2025-01-23-MaeRainha", data: "2025-01-23", horario: "19h", local: "Mãe Rainha", padre: "Padre Ivan" },
 
     { id: "78-2025-01-24-18h-Horizonte-SantaDulce", data: "2025-01-24", horario: "18h", local: "Horizonte na Comunidade de Santa Dulce (Casamento da Thays)", padre: "Padre Eudásio" },
     { id: "78-2025-01-24-19h-ConegoPinto", data: "2025-01-24", horario: "19h", local: "Cônego Pinto", padre: "Padre Ivan" },
