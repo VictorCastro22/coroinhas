24a72444b181ae9fb2bded5883b30962353ec26c
Victor Manuel <victor3mcastro@gmail.com>
2025-02-25 19:37:19 -0300

    Att

--- src/componentes/CalendarioPadres/index.tsx
+++ src/componentes/CalendarioPadres/index.tsx
@@ -154,7 +154,7 @@ const CalendarioPadres: React.FC = () => {
     { id: "78-2025-03-12-19h-AtendimentoSecretaria", data: "2025-03-12", horario: "08h", local: "Atendimento na Secretaria", padre: "Padre Eudásio" },
     { id: "78-2025-03-12-17h-ConfissoesMatriz", data: "2025-03-12", horario: "17h", local: "Confissões na Matriz", padre: "Padre Rafael" },
     { id: "78-2025-03-12-19h-SantosDumont", data: "2025-03-12", horario: "19h", local: "Santos Dumont", padre: "Padre Ivan" },
-    { id: "78-2025-03-12-19h-MissaFamilias", data: "2025-03-12", horario: "19h", local: "Matriz", padre: "Padre Rafael" },
+    { id: "78-2025-03-12-19h-MissaFamilias", data: "2025-03-12", horario: "19h", local: "Missa pelas famílias", padre: "Padre Rafael" },
 
     { id: "78-2025-03-13-08h-ConfissoesMatriz", data: "2025-03-13", horario: "08h", local: "Confissões na Matriz", padre: "Padre Ivan" },
     { id: "78-2025-03-13-12h-Matriz", data: "2025-03-13", horario: "12h", local: "Matriz", padre: "Padre Eudásio" },
@@ -247,12 +247,11 @@ const CalendarioPadres: React.FC = () => {
     { id: "78-2025-03-30-07h-Matriz", data: "2025-03-30", horario: "07h", local: "Matriz", padre: "Padre Eudásio" },
     { id: "78-2025-03-30-07h-Divino", data: "2025-03-30", horario: "07h", local: "Divino", padre: "Padre Ivan" },
     { id: "78-2025-03-30-09h-Matriz", data: "2025-03-30", horario: "09h", local: "Matriz", padre: "Padre Eudásio" },
-    { id: "78-2025-03-30-09h-EncontroPastoral", data: "2025-03-30", horario: "09h", local: "Encontro de Pastoral", padre: "Padre Rafael" },
     { id: "78-2025-03-30-17h-PqSaoJoao", data: "2025-03-30", horario: "17h", local: "Parque São João", padre: "Padre Ivan" },
     { id: "78-2025-03-30-17h-CentroPastoral", data: "2025-03-30", horario: "17h", local: "Centro de Pastoral", padre: "Padre Eudásio" },
     { id: "78-2025-03-30-17h-Divino", data: "2025-03-30", horario: "17h", local: "Divino", padre: "Padre Rafael" },
     { id: "78-2025-03-30-19h-NPqIracema", data: "2025-03-30", horario: "19h", local: "Novo Parque Iracema", padre: "Padre Ivan" },
-    { id: "78-2025-03-30-19h-PqSaoJoao", data: "2025-03-30", horario: "19h", local: "Parque São João", padre: "Padre Rafael" }
+    { id: "78-2025-03-30-19h-PqSaoJoao", data: "2025-03-30", horario: "19h", local: "Matriz", padre: "Padre Rafael" }
 
   ];
 
