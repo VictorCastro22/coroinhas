45702202de8a147cbcbcd9a9455dde116ded2c27
Victor Manuel <victor3mcastro@gmail.com>
2025-06-30 20:01:00 -0300

    aTT

--- src/dados/escala.tsx
+++ src/dados/escala.tsx
@@ -25,5 +25,30 @@ export const escala = [
   { id: "78-2025-07-06-17h-Divino", data: "2025-07-06", horario: "17h", local: "Divino", padre: "Padre Rafael" },
   { id: "80-2025-07-06-19h-SaoPedro", data: "2025-07-06", horario: "19h", local: "São Pedro - Festa de São Pedro", padre: "Padre Eudásio" },
   { id: "78-2025-07-06-19h-Matriz", data: "2025-07-06", horario: "19h", local: "Matriz", padre: "Padre Rafael" },
-  { id: "78-2025-07-06-19h-NovoPqIracema", data: "2025-07-06", horario: "19h", local: "Novo Parque Iracema", padre: "Padre Ivan" }
+  { id: "78-2025-07-06-19h-NovoPqIracema", data: "2025-07-06", horario: "19h", local: "Novo Parque Iracema", padre: "Padre Ivan" },
+  { id: "78-2025-07-07-19h-SantosDumont", data: "2025-07-07", horario: "19h", local: "Santos Dumont", padre: "Padre Ivan" },
+
+  { id: "78-2025-07-08-19h-Matriz", data: "2025-07-08", horario: "19h", local: "Matriz - Missa Votiva", padre: "Padre Ivan" },
+
+  { id: "78-2025-07-09-19h-MissaFamilias", data: "2025-07-09", horario: "19h", local: "Missa pelas Famílias", padre: "Padre Rafael" },
+  { id: "78-2025-07-09-19h-SantosDumont", data: "2025-07-09", horario: "19h", local: "Santos Dumont", padre: "Padre Ivan" },
+
+  { id: "78-2025-07-10-19h-Pirapora", data: "2025-07-10", horario: "19h", local: "Pirapora", padre: "Padre Rafael" },
+  { id: "78-2025-07-10-19h-MaeRainha", data: "2025-07-10", horario: "19h", local: "Mãe Rainha", padre: "Padre Ivan" },
+
+  { id: "80-2025-07-12-07h30-DiaDMissionarioUrucara", data: "2025-07-12", horario: "17h", local: "Urucará - Dia 'D' Missionário", padre: "Padre Eudásio" },
+  { id: "78-2025-07-12-19h-Matriz", data: "2025-07-12", horario: "19h", local: "Matriz", padre: "Padre Rafael" },
+  { id: "78-2025-07-12-19h-OutraBanda", data: "2025-07-12", horario: "19h", local: "Outra Banda", padre: "Padre Ivan" },
+
+  { id: "80-2025-07-13-07h-Matriz", data: "2025-07-13", horario: "07h", local: "Matriz", padre: "Padre Eudásio" },
+  { id: "78-2025-07-13-07h-Abrigo", data: "2025-07-13", horario: "07h", local: "Abrigo", padre: "Padre Rafael" },
+  { id: "78-2025-07-13-07h-Divino", data: "2025-07-13", horario: "07h", local: "Divino", padre: "Padre Ivan" },
+  { id: "80-2025-07-13-09h-Matriz", data: "2025-07-13", horario: "09h", local: "Matriz", padre: "Padre Eudásio" },
+  { id: "78-2025-07-13-12h-Matriz", data: "2025-07-13", horario: "12h", local: "Matriz", padre: "Padre Rafael" },
+  { id: "80-2025-07-13-16h-Vilares", data: "2025-07-13", horario: "16h", local: "Vilares", padre: "Padre Eudásio" },
+  { id: "78-2025-07-13-17h-CentroPastoral", data: "2025-07-13", horario: "17h", local: "Centro de Pastoral", padre: "Padre Rafael" },
+  { id: "80-2025-07-13-17h-Divino", data: "2025-07-13", horario: "17h", local: "Divino", padre: "Padre Eudásio" },
+  { id: "78-2025-07-13-17h-PqSaoJoao", data: "2025-07-13", horario: "17h", local: "Parque São João", padre: "Padre Ivan" },
+  { id: "78-2025-07-13-19h-NovoPqIracema", data: "2025-07-13", horario: "19h", local: "Novo Parque Iracema", padre: "Padre Ivan" },
+
 ];
