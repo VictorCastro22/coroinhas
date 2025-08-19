fdc40423fb20ac84d69031821417de5738e058e4
Victor Manuel <victor3mcastro@gmail.com>
2025-08-19 19:18:37 -0300

    Att

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'], // inclui Safari antigo
+    }),
+  ],
+  build: {
+    target: 'es2015', // compatível com todos os iPhones
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
